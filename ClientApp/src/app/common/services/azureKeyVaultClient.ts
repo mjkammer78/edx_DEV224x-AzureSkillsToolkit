aacb735d6d8d09e7f792a59f872eac64039864c6
Maurits Kammer <mjkammer78@hotmail.com>
2019-11-20 12:35:36 +0100

    reworked key vault secret retrieval through MSI

--- ClientApp/src/app/common/services/azureKeyVaultClient.ts
+++ ClientApp/src/app/common/services/azureKeyVaultClient.ts
@@ -1,43 +1,30 @@
 import { Injectable } from '@angular/core';
-// import { Http, Headers } from '@angular/http';
 // import { KeyVaultClient, KeyVaultCredentials } from 'azure-keyvault';
-import { AuthenticationContext } from 'adal-node';
 import KeyVault = require('azure-keyvault');
+import { Http } from '@angular/http';
+const msRestAzure = require('ms-rest-azure');
+import { loginWithAppServiceMSI, MSIAppServiceTokenCredentials } from 'ms-rest-azure'; 
+import { ServiceClientCredentials } from 'ms-rest';
 
 @Injectable()
 export class AzureKeyVaultClient {
 
-    private clientId = process.env['AZURE_CLIENT_ID'];
-    private secret = process.env['AZURE_CLIENT_SECRET'];
-
-    private secretVault = 'AzureToolkitVault';
-
-    private kvCreds = new KeyVault.KeyVaultCredentials(this.adalCallback, null);
-    public keyVaultClient = new KeyVault.KeyVaultClient(this.kvCreds);
+    private secretVault = 'https://azuretoolkitvault.vault.azure.net';  // could be moved to ENV variable
 
     public async getSecret(secretname: string): Promise<string> {
-        // secretname = 'BingSearchApiKey';
-        const secretVersion = '';
-        const result = await this.keyVaultClient.getSecret(this.secretVault, secretname, secretVersion);
+
+        const credentials = await this.getKeyVaultCredentials();
+        const result = await this.getKeyVaultSecret(credentials, secretname);
+
         return result.value;
     }
 
-    // Callback for ADAL authentication.
-    private adalCallback(challenge: any, callback: any) {
-
-        const context = new AuthenticationContext(challenge.authorization);
-
-        return context.acquireTokenWithClientCredentials(challenge.resource, this.clientId, this.secret,
-            (err: any, tokenResponse: any) => {
-
-                if (err) {
-                    throw err;
-                }
-
-                // Calculate the value to be set in the request's Authorization header and resume the call.
-                return callback(null, tokenResponse.tokenType + ' ' + tokenResponse.accessToken);
-            });
-
+    private getKeyVaultCredentials(): Promise<MSIAppServiceTokenCredentials> {
+        return loginWithAppServiceMSI({resource: 'https://vault.azure.net'});
     }
 
+    private getKeyVaultSecret(credentials: ServiceClientCredentials, secretname: string) {
+        const keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+        return keyVaultClient.getSecret(this.secretVault, secretname, '');
+    }
 }
